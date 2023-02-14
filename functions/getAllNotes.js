2ac412a04b089dc62e9906afa4fb76a8837aad57
aksharma-7 <atulkumar.sharma711@gmail.com>
2023-02-14 14:42:19 +0530

    scanning db to get all notes

--- functions/getAllNotes.js
+++ functions/getAllNotes.js
@@ -1,10 +1,23 @@
-"use strict";
+const DynamodDB = require("aws-sdk/clients/dynamodb");
+const documentClient = new DynamodDB.DocumentClient({ region: "us-east-1" });
+const { NOTES_TABLE_NAME } = process.env;
 
 const getAllNotes = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify("Fetched All notes"),
-  };
+  try {
+    const params = {
+      TableName: NOTES_TABLE_NAME,
+    };
+    const notes = await documentClient.scan(params).promise();
+    return {
+      statusCode: 200,
+      body: JSON.stringify(notes),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error.message),
+    };
+  }
 };
 
 module.exports = { handler: getAllNotes };
