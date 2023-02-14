1171b18c1f955613bb25d52b43bb701dd524e82c
aksharma-7 <atulkumar.sharma711@gmail.com>
2023-02-14 14:34:49 +0530

    added delete notes logic

--- functions/deleteNote.js
+++ functions/deleteNote.js
@@ -1,11 +1,28 @@
-"use strict";
+const DynamodDB = require("aws-sdk/clients/dynamodb");
+const documentClient = new DynamodDB.DocumentClient({ region: "us-east-1" });
+const { NOTES_TABLE_NAME } = process.env;
 
 const deleteNote = async (event) => {
   const notesId = event.pathParameters.id;
-  return {
-    statusCode: 200,
-    body: JSON.stringify(`The Note with ${notesId} has been deleted`),
-  };
+  try {
+    const params = {
+      TableName: NOTES_TABLE_NAME,
+      Key: { notesId },
+      ConditionExpression: "attribute_exists(notesId)",
+    };
+
+    await documentClient.delete(params).promise();
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(`The Note with id: ${notesId} has been deleted`),
+    };
+  } catch (error) {
+    return {
+      statusCode: 500,
+      body: JSON.stringify(error.message),
+    };
+  }
 };
 
 module.exports = { handler: deleteNote };
